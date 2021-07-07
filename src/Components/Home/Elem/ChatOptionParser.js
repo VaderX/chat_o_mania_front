840d9fb3432e1f8c37f3b821402221920a9efbda
VaderX <shuklapriyanshu9@gmail.com>
2021-07-07 20:31:37 +0530

    css updated

---
+++ src/Components/Home/Elem/ChatOptionParser.js
@@ -0,0 +1,31 @@
+import Logo from './../../../logo.svg';
+import React from 'react';
+import { Row, Col, Button } from 'reactstrap';
+
+const ChatOptionParser = (props) => {
+    return (
+        <React.Fragment>
+            <Row className="ChatOptionsHome p-4 mt-5 mb-5">
+                <Col md="4" className="m-auto">
+                    <Row>
+                        <Col md="12" className="fs-1 fw-bold text-primary">
+                            {props.heading}
+                        </Col>
+                        <Col className="text-secondary">
+                            {props.desc}
+                        </Col>
+                    </Row>
+                </Col>
+                <Col md="4" className="m-auto">
+                    <img src={Logo} style={{ width: "250px", height: "100px" }} />
+                    {/* <img src={Logo} /> */}
+                </Col>
+                <Col className="m-auto">
+                    <Button className="RoomBtn">Click Here to enter</Button>
+                </Col>
+            </Row>
+        </React.Fragment>
+    )
+}
+
+export default ChatOptionParser;
---
+++ src/Components/Menu/Menu.js
@@ -0,0 +1,43 @@
+import './Menu.css';
+import React, { useState } from 'react';
+import { Container, Row, Col } from 'reactstrap';
+import { useSelector } from 'react-redux';
+import CreateRoom from '../CreateRoom/CreateRoom';
+import JoinRoom from '../JoinRoom/JoinRoom';
+const Menu = () => {
+    const roomId = useSelector(state => state.roomId)
+    const [create, setCreate] = useState(false);
+    const [join, setJoin] = useState(false);
+    const backHandler = () => {
+        setCreate(false); setJoin(false);
+    }
+    // console.log("room id from home", roomId)
+    // console.log("this is user hehehe", currentUser)
+    if (roomId)
+        window.location = '/room/' + roomId;
+
+    return (
+        <React.Fragment>
+            <Container>
+                <Row className="text-center mt-5">
+                    <Col md="3"></Col>
+                    {
+                        (!join && !create) ?
+                            <React.Fragment>
+                                <Col xs="11" md="3" className="m-auto RoomBtn CreateRoomBtn p-5" onClick={() => setCreate(!create)}>
+                                    Create a Room
+                                </Col>
+                                <Col xs="11" md="3" className="m-auto RoomBtn JoinRoomBtn p-5" onClick={() => setJoin(!join)} >
+                                    Join a Room
+                                </Col>
+                            </React.Fragment>
+                            : (!join) ? <CreateRoom back={backHandler} /> : <JoinRoom back={backHandler} />
+                    }
+                    <Col md="3"></Col>
+                </Row>
+            </Container>
+        </React.Fragment>
+    )
+}
+
+export default Menu;
